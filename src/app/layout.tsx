2e7a2f10174b5d40ded96c3f6e6bbc67d45e6dd1
imran pasha <imranpasha8225@gmail.com>
2024-12-08 20:01:39 +0530

    v1.6

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,6 +59,7 @@ export default function RootLayout({
         <meta name="author" content="Urban Service Company" />
         <meta name="robots" content="index, follow" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="google-adsense-account" content="ca-pub-2914201371636948">
         <link rel="icon" href="icon2.png" />
         <style>
           {`
